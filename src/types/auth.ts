f12299272b49fb13779bae18300afe1647ab7b44
Thi4go0077 <patzithiago@donboscosucre.edu.bo>
2026-08-19 00:24:47 +0200

    proyecto1

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,20 +1,20 @@
-export type UserRole = "ADMIN" | "USUARIO"
+export type UserRole = "ADMIN" | "USUARIO";
 
 
 export interface User {
-    id: string;
-    name: string;
-    carnet: string;
-    role: UserRole;
+  id: string;
+  name: string;
+  carnet: string;
+  role: UserRole;
 }
 
 
-export interface UserRecord extends User{
-    password: string;
+export interface UserRecord extends User {
+  password: string;
 }
 
 
 export interface LoginCredentials {
-    carnet: string;
-    password: string;
+  carnet: string;
+  password: string;
 }
